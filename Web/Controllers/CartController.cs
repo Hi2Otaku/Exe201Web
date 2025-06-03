185c5495eb0d28312691916fcb7946492863bda3
Hi2Otaku <hi2otaku@gmail.com>
2025-06-03 20:46:22 +0700

    Commit

---
+++ Web/Controllers/CartController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+	public class CartController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+	}
+}
---
+++ Web/Controllers/ProfileController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+	public class ProfileController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+		public IActionResult Notification()
+		{
+			return View();
+		}
+	}
+}
